a563e132b243e1fad13d398233c68dcabafebcd7
Pavel <plat956@gmail.com>
2022-04-21 11:19:07 +0300

    Simple entity audit(persist/update) added using JPA features

--- model/src/main/java/com/epam/esm/entity/BaseEntity.java
+++ model/src/main/java/com/epam/esm/entity/BaseEntity.java
@@ -1,15 +1,15 @@
 package com.epam.esm.entity;
 
+import com.epam.esm.listener.AuditListener;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @MappedSuperclass
+@EntityListeners(AuditListener.class)
 @Data
 @NoArgsConstructor
 public abstract class BaseEntity<T> implements Serializable {
@@ -17,4 +17,12 @@ public abstract class BaseEntity<T> implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private T id;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String operation;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private Long timestamp;
 }
