f5f9d2d9bf2662fcc98612f537f77671c394740c
Pavel <plat956@gmail.com>
2022-05-31 20:40:53 +0300

    prepared to CI/CD

---
+++ web/src/main/java/com/epam/esm/RestApplication.java
@@ -0,0 +1,20 @@
+package com.epam.esm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RestApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestApplication.class);
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
+}
